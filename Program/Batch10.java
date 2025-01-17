19ca017f423866cdcd435202be98e0ff132de26e
dipteerajpoot <rajputdiptee1538@gmail.com>
2025-01-17 18:18:00 +0530

    First Program

---
+++ Program/Batch10.java
@@ -0,0 +1,13 @@
+package party;
+public class Batch10
+{
+	public Batch10()
+	{
+		System.out.println("Only Assignment");
+	}
+	public void display()
+	{
+		System.out.println(" Assignment Done");
+	}
+}
+
